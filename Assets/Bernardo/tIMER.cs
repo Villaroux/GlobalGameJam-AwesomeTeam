e4d5e93ab5fb497fea95762d5679550cc5011109
Villaroux <37813904+Villaroux@users.noreply.github.com>
2019-01-26 11:22:59 +0000

    Timer + Game Manager

--- Assets/Bernardo/tIMER.cs
+++ Assets/Bernardo/tIMER.cs
@@ -1,11 +1,21 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-public class tIMER : MonoBehaviour
+﻿using UnityEngine;
+using TMPro;
+public class Timer : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    TextMeshProUGUI timerText;
+
+    float timer;
+    [SerializeField]
+    [Tooltip("In minutes")]
+    float levelTimer = 0.0f;
+
+    private void Awake()
+    {
+        timerText = GetComponent<TextMeshProUGUI>();
+        timer = levelTimer * 60.0f;
+    }
+
+    private void Start()
     {
         
     }
@@ -13,6 +23,25 @@ public class tIMER : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        timer -= Time.deltaTime * 100;
+
+        float minutes = Mathf.Floor(timer / 60.0f);
+
+        float seconds = Mathf.RoundToInt(timer % 60);
+
+        if (timer < 0.0f)
+        {
+            minutes = 0.0f;
+            seconds = 0.0f;
+
+            if (!GameManager._instance.stage1EndScreen.activeInHierarchy)
+            {
+                GameManager._instance.YouLose();
+            }
+        }
+        if (timerText != null)
+        {
+            timerText.text = minutes.ToString("00") + ":" + seconds.ToString("00");
+        }
     }
 }
